aba47e17b8a52535413c84a40cb866f99d91a554
maxxtron <maxxtron93@gmail.com>
2024-02-29 15:54:30 +0200

    add effector

---
+++ components/templates/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container header__container'>
+        <button className='btn-reset header__burger'>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ hooks/useLang.ts
@@ -0,0 +1,10 @@
+import { useUnit } from "effector-react"
+import { $lang } from "@/context/lang"
+import translationJson from '@/public/translations/translations.json'
+
+export const useLang = () => {
+    const lang = useUnit($lang)
+    const translations = translationJson
+
+    return { lang, translations}
+}
